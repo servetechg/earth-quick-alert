056cfc96e1bb112736d57f7650d7bb40a98b4245
harryshk <harisnadeemshk@gmail.com>
2026-05-07 00:47:26 -0700

    Heat map implementation

--- lib/constants/google-maps-config.ts
+++ lib/constants/google-maps-config.ts
@@ -4,6 +4,6 @@
 
 export const GOOGLE_MAPS_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "AIzaSyBq2yjNcUbJoOOwyLa3HzO4xRPVGD9EQI4";
 
-export const GOOGLE_MAPS_LIBRARIES: ("places" | "drawing" | "geometry" | "visualization")[] = ["places"];
+export const GOOGLE_MAPS_LIBRARIES: ("places" | "drawing" | "geometry" | "visualization")[] = ["places", "visualization"];
 
 export const GOOGLE_MAPS_LOADER_ID = "google-map-script";
